90bf7de71fa4cb8402deac834d0e7acea6bea2c5
Công Dũng Phan <phancd@phancds-MacBook-Pro.local>
2024-10-11 20:22:11 +0700

    add musicplay

--- assets/js/autoPlayMusic.js
+++ assets/js/autoPlayMusic.js
@@ -1,80 +1,12 @@
-//Import music
-var nhacNen = new Audio('/src/sounds _music/solo.mp3')
-//get Id music
-// let music = document.getElementById('music')
+$(document).ready(function() {
+   // Lấy thẻ audio
+   var nhacNen = document.getElementById('nhacNen');
 
-// let music2 = document.getElementById('bodyMu') 
-
-function checkNhac() {
-if( $('#batTatNhac').hasClass('nhacBat')) {
-   return true;
-}
-}
-
-$(document).click(function(e) {
-   if($(e.target).closest('.bodyMu').length == 0) {
-
-if ($('#batTatNhac').hasClass('nhacBat')) 
-   
-   $('#batTatNhac').removeClass('nhacBat');
-   
-   
-   console.log('da chay');
-   $('#notiPhatNhac').removeClass('an');
-   $('#notiPhatNhac').addClass('hien');
-  
-   if (typeof nhacNen.loop == 'boolean')
-{
-    nhacNen.loop = true;
-}
-else
-{
-    nhacNen.addEventListener('ended', function() {
-        this.currentTime = 175;
-        this.play();
-    }, false);
-} nhacNen.play();
-
-}
-   else if(!checkNhac()) { {
-    
-      console.log('da tat');
-      nhacNen.pause();
-      $('#notiPhatNhac').removeClass('hien');
-      $('#notiPhatNhac').addClass('an');
-      $('#batTatNhac').addClass('nhacBat');
-   
-      
-    }
-   }
-}) 
-
-
-
-//    }
-//    $('#batTatNhac').removeClass('nhacBat');
-// } else
-// music.addEventListener('click', () => {
-         // nhacNen.play();
-       
-      // })
-
-// let music2 = document.getElementById('bodyMu') 
-
-// music2.addEventListener('click', () => {
-//           check = () => {if(nhacNen.play()) {
-//             return true;
-//          }
-           
-//           }
-//     if(check() == 0) {
-//                nhacNen.pause();
-//             } else
-//    nhacNen.play();
-//             console.log("da chay");
-            
-            
-// })
-// $(document).click(function(e) {
-  
-//  })
+   // Bắt sự kiện click vào toàn bộ body
+   $('body').one('click', function() {
+     // Phát nhạc khi người dùng click vào trang
+     nhacNen.play();
+     nhacNen.loop = true;
+     console.log("Nhạc đang phát");
+   });
+ });
